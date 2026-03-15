caea2bcdbca295744b63d3d2fa17ac796399ef1c
Cindy Pessoa <cindypessoa17@gmail.com>
2026-03-15 14:58:36 -0300

    Update exception

---
+++ src/main/java/com/projetoqualis_cindy/qualis_api/exception/AreaNotFoundException.java
@@ -0,0 +1,7 @@
+package com.projetoqualis_cindy.qualis_api.exception;
+
+public class AreaNotFoundException extends RuntimeException {
+    public AreaNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/projetoqualis_cindy/qualis_api/exception/PeriodicoNotFoundException.java
+++ src/main/java/com/projetoqualis_cindy/qualis_api/exception/PeriodicoNotFoundException.java
@@ -1,6 +1,6 @@
 package com.projetoqualis_cindy.qualis_api.exception;
 
-public class PeriodicoNotFoundException {
+public class PeriodicoNotFoundException extends RuntimeException {
     public PeriodicoNotFoundException(String message) {
         super(message);
 
