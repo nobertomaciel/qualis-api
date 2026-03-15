30750e04f222d3d4c4b45faf4ff36c9302eef653
Cindy Pessoa <cindypessoa17@gmail.com>
2026-03-15 14:53:53 -0300

    Create PeriodicoNotFound

--- src/main/java/com/projetoqualis_cindy/qualis_api/exception/PeriodicoNotFoundException.java
+++ src/main/java/com/projetoqualis_cindy/qualis_api/exception/PeriodicoNotFoundException.java
@@ -1,5 +1,9 @@
 package com.projetoqualis_cindy.qualis_api.exception;
 
 public class PeriodicoNotFoundException {
-    
+    public PeriodicoNotFoundException(String message) {
+        super(message);
+
+    }
+
 }
